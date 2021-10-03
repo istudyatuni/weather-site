2c304d2e201ce89dd375ffd0c38ab4dd11525c67
Ilia <istudyatuni@gmail.com>
2021-10-03 12:22:21 +0300

    Setup and run prettier

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import './global.css';
+import './global.css'
 
-import App from './App.svelte';
+import App from './App.svelte'
 
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
-	}
-});
+		name: 'world',
+	},
+})
 
-export default app;
+export default app
