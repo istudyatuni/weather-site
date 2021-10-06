27a7e29df5c0644ac3f6003198be628088f5d39d
Ilia <istudyatuni@gmail.com>
2021-10-06 23:05:25 +0300

    Deploy script (almost unchanged from tasker)

---
+++ scripts/deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -e
+
+export LANG=en_US.utf-8
+
+repo="https://github.com/istudyatuni/weather-site"
+
+hash=$(git rev-parse HEAD)
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+# build
+
+yarn build
+
+echo
+
+cp -rf public docs
+
+git checkout gh-pages
+
+trap "echo -e '\nExiting\n' && git checkout $branch" EXIT
+
+# rm -rf docs
+# mv public docs
+git add -f docs
+
+echo
+
+git commit --amend -m "Update on $(date -R -u)
+
+Commit $repo/tree/$hash, branch '$branch'"
+
+git checkout $branch
+
+# push
+
+echo
+
+git push -f origin gh-pages
