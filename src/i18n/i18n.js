71d3adcec9ad1a5813f6be72da21750869b36d2c
Ilia <istudyatuni@gmail.com>
2021-10-31 14:37:12 +0300

    Fix loading json

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,15 +1,13 @@
 import { register, init, getLocaleFromNavigator } from 'svelte-i18n'
 
 async function loader(path) {
-	return fetch(path)
+	return (await fetch(path)).json()
 }
 
 const locales_dir = 'locales/'
 
-// import en from './locales/en.json'
-
-register('en', loader(locales_dir + 'en.json'))
-register('ru', loader(locales_dir + 'ru.json'))
+register('en', () => loader(locales_dir + 'en.json'))
+register('ru', () => loader(locales_dir + 'ru.json'))
 
 init({
 	fallbackLocale: 'en',
