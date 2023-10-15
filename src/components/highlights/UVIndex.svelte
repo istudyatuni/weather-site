e5f123708f369aad553d188cbcc0b7cf64d665a8
Ilia <istudyatuni@gmail.com>
2023-10-15 13:49:33 +0300

    fix: better gradient for uv scale

--- src/components/highlights/UVIndex.svelte
+++ src/components/highlights/UVIndex.svelte
@@ -8,7 +8,7 @@
 	export let uv = 0
 
 	let bar_width_px = 0
-	$: uv_scale = (bar_width_px - 6) / 11
+	$: uv_scale = (bar_width_px - 6) / 12
 </script>
 
 <BaseHighlight
@@ -53,18 +53,29 @@
 		height: 2em;
 		border-radius: 5px;
 
-		// https://cssgradient.io
-		//
 		// percents are calculated this way (in kalker):
-		// (2, 5, 7, 10) * (100 / 11) = (18 + 2/11, 45 + 5/11, 63 + 7/11, 90 + 10/11)
+		// >> 100/13
+		// 7.6923076923 ≈ 7 + 9/13
+		// >> (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12) * 7.7
+		// (7.7, 15 + 2/5, 23.1, 30 + 4/5, 38 + 1/2, 46 + 1/5, 53.9, 61 + 3/5, 69.3, 77, 84.7, 92 + 2/5)
+		//
+		// colors from: https://www.sciencedirect.com/science/article/pii/S2666469023000210, Appendix C
 		background: linear-gradient(
 			90deg,
-			rgba(40, 255, 39, 1) 0%,
-			rgba(255, 252, 33, 1) 18.2%,
-			rgba(255, 153, 0, 1) 45.5%,
-			rgba(255, 91, 3, 1) 63.7%,
-			rgba(255, 20, 0, 1) 90.9%,
-			rgba(204, 0, 255, 1) 100%
+			#658d1b 0%, // duplicate for start
+			#658d1b 7.7%, // 0 <= UVI < 1
+			#84bd00 15.4%, // 1 <= UVI < 2
+			#97d700 23.1%, // 2 <= UVI < 3
+			#f7ea48 30.8%, // 3 <= UVI < 4
+			#fce300 38.5%, // 4 <= UVI < 5
+			#ffcd00 46.2%, // 5 <= UVI < 6
+			#eca154 53.9%, // 6 <= UVI < 7
+			#ff8200 61.6%, // 7 <= UVI < 8
+			#ef3340 69.3%, // 8 <= UVI < 9
+			#da291c 77%, // 9 <= UVI < 10
+			#bf0d3e 84.7%, // 10 <= UVI < 11
+			#4b1e88 92.4%, // 11 <= UVI < 12
+			#62359f 100%, // 12 <= UVI < 13
 		);
 	}
 </style>
