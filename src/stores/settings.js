5d4209f99e2536ced6bf8539f010f120456e03ee
Ilia <istudyatuni@gmail.com>
2023-10-15 21:29:52 +0300

    feat: set maps provider to google by default on mobiles

--- src/stores/settings.js
+++ src/stores/settings.js
@@ -1,6 +1,7 @@
 import { localStore } from 'svelte-storages'
 
 import { migrateStore } from 'src/stores/migration'
+import isMobile from 'is-mobile'
 
 const initial = {
 	version: 2,
@@ -14,7 +15,8 @@ const initial = {
 	// get client timezone
 	tz: Intl.DateTimeFormat().resolvedOptions().timeZone,
 	/** @type {import('src/utils/types').OsmProvider} */
-	maps_provider: 'osm',
+	// set to google on mobile to support opening links in mobile app
+	maps_provider: isMobile({ tablet: true }) ? 'google' : 'osm',
 }
 
 /** Settings store */
